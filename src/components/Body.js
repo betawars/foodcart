eb24eb931eb1b5157c673022d57e21227841185b
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-05-11 17:59:49 -0700

    commit-5: Added 1 simple filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = (props) => {
 
     const{resList} = props
+
+    const [mainList, setMainList] = useState(resList)
+
+    const onFilterClick = () => {
+        setMainList(mainList.filter((e)=>e.info.rating.rating_text>=4.5))
+    }
+
+    const onClearFilter = () => {
+        setMainList(resList)
+    }
+
     return(
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button onClick={onFilterClick} className="filter-btn">Top Rated Restaurants</button>
+                <button onClick={onClearFilter} className="filter-btn">Clear Filter</button>
+            </div>
             <div className="res-container">
-                {resList.map((data)=><div key={data.info.resId}><RestaurantCard data = {data}/></div>)}
+                {mainList.map((data)=><div key={data.info.resId}><RestaurantCard data = {data}/></div>)}
             </div>
         </div>
     )
