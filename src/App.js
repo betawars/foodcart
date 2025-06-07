4d0911c303fc4fb164d57c3649f9fc1307dd6f12
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-06-06 19:23:57 -0700

    commit-16: Reformmeted the code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "../index.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-import { resList } from "./utils/mockData";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
-// import Contact from "./components/Contact";
 import Error from "./components/Error";
 import AboutBody from "./components/AboutBody";
 import AboutBody1 from "./components/AboutBody1";
 import AboutBody2 from "./components/AboutBody2";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Shimmer from "./components/Shimmer";
-// import Grocery from "./components/Grocery";
 const Grocery = lazy(()=>import("./components/Grocery"));
 const Contact = lazy(()=>import("./components/Contact"));
 
