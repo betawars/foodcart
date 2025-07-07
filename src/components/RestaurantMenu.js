6a418f4d26fa924a51b310c447bcaee5da2032d6
betawars@oregonstate.edu <betawars@oregonstate.edu>
2025-07-06 20:46:27 -0700

    commit-51: Changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -8,7 +8,7 @@ import RestaurantCategory from "./RestaurantCategory";
 const RestaurantMenu = () => {
     const resId = useParams();
     const resInfo = useRestaurantMenu(resId.resId);
-    console.log(resInfo)
+
     if (!resInfo) return <ShimmerMenu />;
 
     const { name, costForTwoMessage, cuisines, cloudinaryImageId } = resInfo
